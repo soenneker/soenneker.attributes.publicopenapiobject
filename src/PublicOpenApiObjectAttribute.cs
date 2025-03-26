fba485a6ebccf73053475b3d36f6cbb188318495
Jake Soenneker <jake@soenneker.com>
2025-03-26 09:20:44 -0500

    Initial commit

---
+++ src/PublicOpenApiObjectAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Soenneker.Attributes.PublicOpenApiObject;
+
+/// <summary>
+/// For decorating objects that should be publicly accessible to see within OpenApi
+/// </summary>
+[AttributeUsage(AttributeTargets.Class)]
+public class PublicOpenApiObjectAttribute : Attribute
+{
+}
---
+++ test/Soenneker.Attributes.PublicOpenApiObject.Tests/PublicOpenApiObjectAttributeTests.cs
@@ -0,0 +1,13 @@
+﻿using Soenneker.Tests.Unit;
+using Xunit;
+
+namespace Soenneker.Attributes.PublicOpenApiObject.Tests;
+
+public sealed class PublicOpenApiObjectAttributeTests : UnitTest
+{
+    [Fact]
+    public void Default()
+    {
+
+    }
+}
