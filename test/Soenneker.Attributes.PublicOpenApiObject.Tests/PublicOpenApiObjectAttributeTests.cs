305eda36703ee33604ff10f990818e72dfde8151
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:12 -0500

    TUnit conversion

--- test/Soenneker.Attributes.PublicOpenApiObject.Tests/PublicOpenApiObjectAttributeTests.cs
+++ test/Soenneker.Attributes.PublicOpenApiObject.Tests/PublicOpenApiObjectAttributeTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Attributes.PublicOpenApiObject.Tests;
 
 public sealed class PublicOpenApiObjectAttributeTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
